301f69fddfffeee4967650eb234fc48199f8bd20
guojj33 <guojj33@mail2.sysu.edu.cn>
2025-02-09 23:00:47 +0800

    update

--- train_model.py
+++ train_model.py
@@ -4,6 +4,7 @@ from torch.nn.functional import cross_entropy
 from torch.optim import Adam
 from torch.optim.lr_scheduler import LambdaLR
 
+import time, os
 from tqdm import tqdm
 import numpy as np
 
@@ -12,17 +13,23 @@ from wiki_feeder import WikiFeeder
 from BPETokenizer import BPETokenizer
 from lr_schedulers import Cosine_Scheduler
 
-tokenizer = BPETokenizer()
-tokenizer.load('./output/tokenizer')
-
+tokenizer_dir = './tokenizer'
+work_dir = './output/train/{}'.format(time.strftime("%Y-%m-%d_%H-%M-%S"))
+os.makedirs(work_dir)
 data_dir = './datasets/wikitext-2-raw-v1'
+
+tokenizer = BPETokenizer()
+tokenizer.load(tokenizer_dir)
+
 train_dataset = WikiFeeder('train', data_dir, tokenizer)
 train_dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True)
 # test_dataset = WikiFeeder('test', data_dir, tokenizer)
 # test_dataloader = DataLoader(test_dataset, batch_size=16)
 device = torch.device('mps:0')
+
+start_epoch = 0
 max_epoch = 100
-warm_up_epoch = 5
+warm_up_epoch = 3
 
 model = mTransformer(tokenizer.vocabulary_size()).to(device)
 optimizer = Adam(model.parameters(), lr=5e-5)
@@ -44,15 +51,40 @@ class meanVar():
   def getMean(self):
     return np.array(self.l).sum()/len(self.l)
 
-for e in range(max_epoch):
+def save_checkpoint(epoch, name='checkpoint'):
+   print('saving {}...'.format(name))
+   checkpoint = {
+      'model': model,
+      'optimizer': optimizer.state_dict(),
+      'scheduler': scheduler.state_dict(),
+      'epoch': epoch
+   }
+   torch.save(checkpoint, '{}/{}.pth.tar'.format(work_dir, name))
+   print('{} saved.'.format(name))
+
+def load_checkpoint(load_path):
+  checkpoint = torch.load(load_path)
+  global model, optimizer, scheduler, start_epoch
+  model = checkpoint['model']
+  optimizer.load_state_dict(checkpoint['optimizer'])
+  scheduler.load_state_dict(checkpoint['scheduler'])
+  start_epoch = checkpoint['epoch']
+  print('resume training: loading from {}'.format(load_path))
+
+# load_path = None
+load_path = './output/train/2025-02-09_22-58-16/checkpoint.pth.tar'
+if not load_path is None:
+   load_checkpoint(load_path)
+
+for e in range(start_epoch, max_epoch):
+    print('[train epoch {}/{}]'.format(e+1, max_epoch))
     model.train()
     train_loss = meanVar()
     train_iter = tqdm(train_dataloader, dynamic_ncols=True)
     for idx, (xs, labels) in enumerate(train_iter):
         optimizer.zero_grad()
-
-        xs = torch.tensor(xs).to(device)  # bz, 1024
-        labels = torch.tensor(labels).to(device)  # bz, 1024
+        xs = xs.to(device)  # bz, 1024
+        labels = labels.to(device)  # bz, 1024
         loss = get_loss(xs, labels)
         train_loss.append(loss.item())
         train_iter.set_description('[loss: {:.4f}, lr: {:.4f}(1e-4)]'.format(loss.item(), optimizer.param_groups[0]['lr']*1e4))
@@ -64,4 +96,4 @@ for e in range(max_epoch):
 
     train_loss = train_loss.getMean()
     print('train epoch: {}, loss: {}'.format(e+1, train_loss))
-    torch.save(model, './output/train/model.pth.tar')
+    save_checkpoint(e+1)
